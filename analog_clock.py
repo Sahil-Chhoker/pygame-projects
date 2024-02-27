a5884b8bfea811a8361ad75ae0b4cf7db0405118
sahil-chhoker <chhokersahil435@gmail.com>
2024-02-27 10:51:37 +0530

    added logic for displaying numbers

--- analog_clock.py
+++ analog_clock.py
@@ -19,6 +19,16 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 
+def number(number, size, position):
+    font = pygame.font.SysFont("Arial", size, True, False)
+    text = font.render(number, True, WHITE)
+    text_rect = text.get_rect(center=(position))
+    screen.blit(text, text_rect)
+
+def polar_to_cartesian(r, theta):
+    x = r * sin(pi * theta / 180)
+    y = r * cos(pi * theta / 180)
+    return x + WIDTH / 2, -(y - HEIGHT / 2)
 
 def main():
     run = True
@@ -30,6 +40,7 @@ def main():
 
         screen.fill(BLACK)
         pygame.draw.circle(screen, WHITE, center, clock_radius - 15, 8)
+        pygame.draw.circle(screen, WHITE, center, 15)
 
         pygame.display.update()
 
