dac1948302c18e393657b5d2d9463d2dae1dd24d
sahil-chhoker <chhokersahil435@gmail.com>
2024-02-27 10:43:37 +0530

    drew a clock

--- analog_clock.py
+++ analog_clock.py
@@ -1,6 +1,10 @@
 import pygame
+from math import cos, sin, pi
+import datetime
 
 HEIGHT, WIDTH = 800, 800
+center = (WIDTH/2, HEIGHT/2)
+clock_radius = 400
 
 pygame.init()
 
@@ -25,11 +29,14 @@ def main():
                 run = False
 
         screen.fill(BLACK)
+        pygame.draw.circle(screen, WHITE, center, clock_radius - 15, 8)
 
         pygame.display.update()
 
         clock.tick(FPS)
 
     pygame.quit()
+
+
 
 main()
