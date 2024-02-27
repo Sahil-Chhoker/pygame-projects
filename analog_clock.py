5557c2c45fc5496fc20da78cf0911d13e56916ab
sahil-chhoker <chhokersahil435@gmail.com>
2024-02-27 10:34:35 +0530

    added screen stats

--- analog_clock.py
+++ analog_clock.py
@@ -1,2 +1,12 @@
 import pygame
 
+HEIGHT, WIDTH = 800, 800
+
+pygame.init()
+
+# screen stats
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Analog Clock")
+clock = pygame.time.Clock()
+FPS = 60
+
