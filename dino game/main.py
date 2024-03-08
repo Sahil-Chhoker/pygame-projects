1b87f7eec2873179da7aae79b40bf2e03ae833b1
sahil-chhoker <chhokersahil435@gmail.com>
2024-03-08 15:06:15 +0530

    basic game initilazition

--- dino game/main.py
+++ dino game/main.py
@@ -1,1 +1,36 @@
 import pygame
+import math
+pygame.init()
+
+# screen stats
+WIDTH, HEIGHT =  800, 800
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Wierd Dino")
+
+# colors
+WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
+BLUE = (100, 149, 237)
+RED = (188, 39, 50)
+DARK_GREY = (80, 78, 81)
+
+FONT = pygame.font.SysFont("comicsans", 16)
+
+def main():
+	run = True
+	clock = pygame.time.Clock()
+
+	while run:
+		clock.tick(60)
+		WIN.fill((0, 0, 0))
+
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				run = False
+
+		pygame.display.update()
+
+	pygame.quit()
+
+
+main()
