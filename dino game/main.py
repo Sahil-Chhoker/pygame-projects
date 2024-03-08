2dcb4c1c4c4651f6c259db86d0c3d94936398914
sahil-chhoker <chhokersahil435@gmail.com>
2024-03-08 15:25:36 +0530

    showing dino sprite on screen

--- dino game/main.py
+++ dino game/main.py
@@ -16,9 +16,26 @@ DARK_GREY = (80, 78, 81)
 
 FONT = pygame.font.SysFont("comicsans", 16)
 
+class Dino(pygame.sprite.Sprite):
+	def __init__(self, pos_x, pos_y):
+		super().__init__()
+		self.sprites = []
+		self.sprites.append(pygame.image.load('C:/MASTER FOLDER/pygame-projects/dino game/assets/dino/dino_2.png'))
+		self.sprites.append(pygame.image.load('C:/MASTER FOLDER/pygame-projects/dino game/assets/dino/dino_3.png'))
+		self.current_sprite = 0
+		self.image = self.sprites[self.current_sprite]
+
+		self.rect = self.image.get_rect()
+		self.rect.topleft = [pos_x, pos_y]
+
 def main():
 	run = True
 	clock = pygame.time.Clock()
+
+	moving_sprites = pygame.sprite.Group()
+	dino = Dino(100, 100)
+
+	moving_sprites.add(dino)
 
 	while run:
 		clock.tick(60)
@@ -28,6 +45,7 @@ def main():
 			if event.type == pygame.QUIT:
 				run = False
 
+		moving_sprites.draw(WIN)
 		pygame.display.update()
 
 	pygame.quit()
